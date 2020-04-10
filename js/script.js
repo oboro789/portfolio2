7cba520fd9b829e72a5f7def0b2e4b2f18b92e73
j.s <munerinnemu789@gmail.com>
2020-04-10 18:43:46 +0900

    NEWS部分モーダル追加(途中)とFB部分少し修正とSHOP LIST部分のサーチ部分調整。朝見せ方について指摘があったので他サイトいろいろと参考にしつつちょっと全体的な見せ方を考えたのちに分別しながら少しページ増やしました。案が中々浮かばなかったりするので他サイト見たりして勉強しようと思います。

--- js/script.js
+++ js/script.js
@@ -33,3 +33,17 @@ $(function(){
     nextArrow:'<button class="slide3-arrow next3-arrow"></button>'
    });
 });
+$(function(){
+    $('.js-modal-open').each(function(){
+        $(this).on('click',function(){
+            var target = $(this).data('target');
+            var modal = document.getElementById(target);
+            $(modal).fadeIn();
+            return false;
+        });
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    }); 
+});
